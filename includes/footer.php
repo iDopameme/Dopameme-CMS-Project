16898f6c752166ace6ff0872692b2fd12c7c5615
Richard J. Walker <Dopameme@outlook.com>
2019-08-17 22:20:24 -0400

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+</div><!-- /.row -->
+
+    </main><!-- /.container -->
+
+    <footer class="blog-footer">
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="bootstrap.js"></script>
+  </body>
+</html>
